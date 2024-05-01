587b0d4097f5745f366a6c935b71c70af0d1acf4
Volodymyr Kravchuk <volodymyr.kravchuk@socialtechnologies.io>
2024-05-01 14:14:54 +0300

    final version

--- g/main.go
+++ g/main.go
@@ -1,1 +1,66 @@
-package g
+package main
+
+import (
+	"fmt"
+	"net"
+	"strconv"
+	"strings"
+	"time"
+)
+
+func main() {
+	ln, err := net.Listen("tcp", ":8081")
+	if err != nil {
+		fmt.Println("Error listening:", err)
+		return
+	}
+	defer ln.Close()
+	fmt.Println("Server for g(x) is listening on port 8081")
+
+	for {
+		// Accepting connections
+		conn, err := ln.Accept()
+		if err != nil {
+			fmt.Println("Error accepting connection:", err)
+			continue
+		}
+
+		go handleConnection(conn)
+	}
+}
+
+func handleConnection(conn net.Conn) {
+	defer conn.Close()
+
+	buf := make([]byte, 1024)
+	n, err := conn.Read(buf)
+	if err != nil {
+		fmt.Println("Error reading:", err)
+		return
+	}
+
+	data := string(buf[:n])
+	parts := strings.Fields(data)
+	if len(parts) != 2 {
+		fmt.Println("Invalid request:", data)
+		return
+	}
+
+	x, err := strconv.Atoi(parts[1])
+	if err != nil {
+		fmt.Println("Invalid value for x:", parts[1])
+		return
+	}
+	fx := calculateFx(x)
+
+	_, err = conn.Write([]byte(fmt.Sprintf("%d", fx)))
+	if err != nil {
+		fmt.Println("Error writing:", err)
+		return
+	}
+}
+
+func calculateFx(x int) int {
+	time.Sleep(2 * time.Second)
+	return x % 3
+}
--- main/main.go
+++ main/main.go
@@ -1,1 +1,63 @@
 package main
+
+import (
+	"fmt"
+	"net"
+	"strconv"
+)
+
+func main() {
+	conn, err := net.Dial("tcp", "localhost:8080")
+	if err != nil {
+		fmt.Println("Error connecting:", err)
+		return
+	}
+	defer conn.Close()
+
+	x := 11
+
+	_, err = conn.Write([]byte(fmt.Sprintf("f %d\n", x)))
+	if err != nil {
+		fmt.Println("Error sending data for calculating f(x):", err)
+		return
+	}
+
+	result := make([]byte, 1024)
+	n, err := conn.Read(result)
+	if err != nil {
+		fmt.Println("Error receiving result of calculating f(x):", err)
+		return
+	}
+	fx := string(result[:n])
+
+	conn1, err := net.Dial("tcp", "localhost:8081")
+	if err != nil {
+		fmt.Println("Error connecting:", err)
+		return
+	}
+	defer conn1.Close()
+	_, err = conn1.Write([]byte(fmt.Sprintf("g %d\n", x)))
+	if err != nil {
+		fmt.Println("Error sending data for calculating g(x):", err)
+		return
+	}
+
+	n, err = conn1.Read(result)
+	if err != nil {
+		fmt.Println("Error receiving result of calculating g(x):", err)
+		return
+	}
+	gx := string(result[:n])
+
+	result = []byte(fmt.Sprintf("%s && %s", fx, gx))
+
+	fint, _ := strconv.Atoi(fx)
+	gint, _ := strconv.Atoi(gx)
+	fmt.Println(fint)
+	fmt.Println(gint)
+	if fint != 0 && gint != 0 {
+		fmt.Println("true")
+	} else {
+		fmt.Println("false")
+	}
+}
